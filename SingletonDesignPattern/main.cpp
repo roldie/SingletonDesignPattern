7636d88ce099126f367182ccb95a2c1376da1386
Harold Serrano <harold.serrano@gmail.com>
2014-08-12 19:02:54 -0700

    Initial Commit

---
+++ SingletonDesignPattern/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  SingletonDesignPattern
+//
+//  Created by Harold Serrano on 8/12/14.
+//  Copyright (c) 2014 Roldie.com. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
